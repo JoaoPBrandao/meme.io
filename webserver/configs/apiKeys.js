4a16270b075d4a53b164a06c374a71babe6deed7
Joaos98 <joaos98@icloud.com>
2019-05-24 11:02:20 -0300

    Atualização do Access Token da API do Imgur.

--- webserver/configs/apiKeys.js
+++ webserver/configs/apiKeys.js
@@ -1,6 +1,6 @@
 //Esse arquivo serve para armazenar e exportar as chaves das APIs integradas (Imgur)
 const apiKeys = {
-    imgurAccessToken: "1aa1d4cc8db34a63ac9280116a8b6c740fbf63a9",
+    imgurAccessToken: "a75f1a2bc64a8f4f3a804883dcc17fcebd3a9077",
     imgurClientID: "dfa60a0c4c22fd3",
     imgurClientSecret: "c0f89136de0a5ba20b5f655ee7445f6be2b35dcc"
 };
